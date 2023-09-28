140d22ea2a50adb628b2cb6cf900ce77dc72c05b
Florian Giest <montyjames.pub@gmail.com>
2023-09-29 00:19:04 +0200

    fixing debugging

---
+++ src/include/thread_debug.h
@@ -0,0 +1,53 @@
+// (c) 2023 Florian Giest
+// This code is licensed under MIT license (see LICENSE.txt for details)
+
+#ifndef _DEBUG_H    // BEGIN INCLUDE GUARD
+#define _DEBUG_H
+
+/*********************************************************************************************************/
+/*                                              DEPENDANICIES                                            */
+/*********************************************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <pthread.h>
+
+#define DEBUG
+
+/*********************************************************************************************************/
+/*                                               DEFINITIONS                                             */
+/*********************************************************************************************************/
+
+/* if DEBUG is defines it enables the macro for logging the steps 
+   of every function which calls these functions/macros */
+#ifdef DEBUG
+ /* pretty simple debug function expaned by macro 'debug()'
+    write DEBUG: at the first place followed by the calling pthread id
+    and a formatstring and its arguments
+ */
+ void xdebug(pthread_t threadid, const char *function, const char *fmt, ...);
+ #define debug(fmt, ...) xdebug(pthread_self(), __func__, fmt, ##__VA_ARGS__)
+ /* same as for 'xdebug()' but for error and also prints errno string if
+    errno is set.
+ */
+ void xdebug_error(pthread_t threadid, const char *function, const char *filename, int line, const char *fmt, ...);
+ #define debug_error(fmt, ...) xdebug_error(pthread_self(), __func__, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+
+/*  void xfree(void *ptr, const char *file, int line);
+ #define free(x) xfree((x), __FILE__, __LINE__)
+ */
+#else
+ #define debug_error(x, ...)
+ #define debug(x, ...)
+
+#endif
+
+/*********************************************************************************************************/
+/*                                           FUNCTION DEFINITIONS                                        */
+/*********************************************************************************************************/
+
+
+#endif
---
+++ src/thread_debug.c
@@ -0,0 +1,79 @@
+// (c) 2023 Florian Giest
+// This code is licensed under MIT license (see LICENSE.txt for details)
+
+/*********************************************************************************************************/
+/*                                              DEPENDANICIES                                            */
+/*********************************************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <pthread.h>
+
+#define DEBUG
+
+/*********************************************************************************************************/
+/*                                               DEFINITIONS                                             */
+/*********************************************************************************************************/
+#ifdef DEBUG
+pthread_mutex_t debug_lock = PTHREAD_MUTEX_INITIALIZER;
+
+extern __thread FILE *thread_log;
+
+extern const char* thread_strerror(int err);
+
+/*********************************************************************************************************/
+/*                                           FUNCTION DEFINITIONS                                        */
+/*********************************************************************************************************/
+
+/*  void xfree(void *ptr, const char *file, int line)
+ {
+   printf("call of free in file %s:%d\n", file, line);
+   free(ptr);
+ } */
+
+void xdebug(pthread_t threadid, const char *function, const char *fmt, ...)
+{
+    FILE *fp = thread_log == NULL ? stdout : thread_log;
+    const char name[16] = { 0 };
+    pthread_getname_np(threadid, name, sizeof name);
+
+    pthread_mutex_lock(&debug_lock);
+    va_list args;
+    va_start(args, fmt);
+
+    fprintf(fp, "DEBUG: %16s[%ld] %s(): ", name, threadid, function);
+    vfprintf(fp, fmt, args);
+    fprintf(fp, "\n");
+    fflush(fp);
+    
+    va_end(args);
+    pthread_mutex_unlock(&debug_lock);
+}
+
+
+void xdebug_error(pthread_t threadid, const char *function, const char *filename, int line, const char *fmt, ...)
+{
+    FILE *fp = thread_log == NULL ? stderr : thread_log;
+    const char name[16] = { 0 };
+    pthread_getname_np(threadid, name, sizeof name);
+
+    pthread_mutex_lock(&debug_lock);
+    int err = errno;
+    va_list args;
+    va_start(args, fmt);
+
+    fprintf(fp, "ERROR: %s[%ld] %s(): ", name, threadid, function);
+    vfprintf(fp, fmt, args);
+    if(err) {
+        fprintf(fp, " (%s)", strerror(errno));
+        errno = 0;
+    }
+    fprintf(fp, " in file %s at line %d\n", filename, line);
+    fflush(fp);
+
+    va_end(args);
+    pthread_mutex_unlock(&debug_lock);
+}
+#endif
